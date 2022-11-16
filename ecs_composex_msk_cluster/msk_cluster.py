97d88abad073c9ab2f7f57efee2c3cff1f904429
John "Preston" Mille <john@ews-network.net>
2022-11-16 14:32:11 +0000

    Working MSK deployment and IAM + network access to cluster

--- ecs_composex_msk_cluster/msk_cluster.py
+++ ecs_composex_msk_cluster/msk_cluster.py
@@ -1,4 +1,3 @@
-#   -*- coding: utf-8 -*-
 #  SPDX-License-Identifier: MPL-2.0
 #  Copyright 2020-2022 John Mille <john@compose-x.io>
 
@@ -9,15 +8,22 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ecs_composex.mods_manager import XResourceModule
     from ecs_composex.common.settings import ComposeXSettings
+    from ecs_composex.mods_manager import ModManager
 
+from ecs_composex.common.logging import LOG
 from ecs_composex.compose.x_resources.network_x_resources import NetworkXResource
+from ecs_composex.resource_settings import link_resource_to_services
 from ecs_composex.vpc.vpc_params import STORAGE_SUBNETS
-from troposphere import GetAtt, Ref
+from troposphere import GetAtt, Ref, Select, Split
 
 from ecs_composex_msk_cluster.msk_cluster_params import (
+    CONTROL_CLOUD_ATTR_MAPPING,
     MSK_CLUSTER_ARN,
+    MSK_CLUSTER_CLIENTS_SHARED_SG,
     MSK_CLUSTER_SG_PARAM,
 )
+
+from .msk_cluster_ecs import handle_kafka_iam_permissions
 
 
 class MskCluster(NetworkXResource):
@@ -34,11 +40,25 @@ class MskCluster(NetworkXResource):
     ):
         super().__init__(name, definition, module, settings)
 
+        self.clients_security_group = None
         self.security_group_param = MSK_CLUSTER_SG_PARAM
+        self.clients_security_group_param = MSK_CLUSTER_CLIENTS_SHARED_SG
         self.subnets_override = STORAGE_SUBNETS
+        self.post_processing_properties: list = [
+            "EncryptionInfo.EncryptionAtRest.DataVolumeKMSKeyId",
+            "BrokerNodeGroupInfo.ClientSubnets",
+        ]
+        self.ref_parameter = MSK_CLUSTER_ARN
+        self.cluster_arn_parameter = MSK_CLUSTER_ARN
+        self.cloud_control_attributes_mapping = CONTROL_CLOUD_ATTR_MAPPING
+
+    @property
+    def cluster_uuid(self):
+        if self.cfn_resource:
+            return Select(1, Split(":cluster/", Ref(self.cfn_resource)))
 
     def init_outputs(self):
-        return {
+        self.output_properties: dict = {
             MSK_CLUSTER_ARN: (self.logical_name, self.cfn_resource, Ref, None),
             MSK_CLUSTER_SG_PARAM: (
                 f"{self.logical_name}{self.security_group_param.title}",
@@ -46,4 +66,40 @@ class MskCluster(NetworkXResource):
                 GetAtt,
                 self.security_group_param.return_value,
             ),
+            MSK_CLUSTER_CLIENTS_SHARED_SG: (
+                f"{self.logical_name}{self.clients_security_group_param.title}",
+                self.clients_security_group,
+                GetAtt,
+                self.clients_security_group_param.return_value,
+            ),
         }
+
+    def to_ecs(self, settings, modules: ModManager, root_stack=None) -> None:
+        """
+        Maps a database service to ECS services
+        """
+        if not self.mappings and self.cfn_resource:
+            for target in self.families_targets:
+                if target[0].service_compute.launch_type != "EXTERNAL":
+                    LOG.warning(
+                        f"{self.stack.title} - {target[0].name} - "
+                        "When using EXTERNAL Launch Type, networking settings cannot be set."
+                    )
+                    client_sg_id = self.add_attribute_to_another_stack(
+                        target[0].stack, self.clients_security_group_param, settings
+                    )
+                    target[0].service_networking.extra_security_groups.append(
+                        client_sg_id["ImportParameter"]
+                    )
+                    target[0].ecs_service.ecs_service.NetworkConfiguration = target[
+                        0
+                    ].service_networking.ecs_network_config
+            if self.cluster_arn_parameter:
+                print("LINKING CLUSTER")
+                link_resource_to_services(
+                    settings,
+                    self,
+                    arn_parameter=self.cluster_arn_parameter,
+                    access_subkeys=["MSKCluster"],
+                )
+            handle_kafka_iam_permissions(self, settings)
--- ecs_composex_msk_cluster/msk_cluster_stack.py
+++ ecs_composex_msk_cluster/msk_cluster_stack.py
@@ -1,4 +1,3 @@
-#  -*- coding: utf-8 -*-
 # SPDX-License-Identifier: MPL-2.0
 # Copyright 2020-2022 John Mille <john@compose-x.io>
 
@@ -10,17 +9,46 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from ecs_composex.mods_manager import XResourceModule
+    from ecs_composex.common.settings import ComposeXSettings
+    from .msk_cluster import MskCluster
 
-from ecs_composex.common import build_template
+from compose_x_common.aws.msk import MSK_CLUSTER_ARN_RE
+from compose_x_common.compose_x_common import keyisset
 from ecs_composex.common.stacks import ComposeXStack
-from ecs_composex.compose.x_resources.helpers import (
-    set_lookup_resources,
-    set_new_resources,
-    set_resources,
+from ecs_composex.common.troposphere_tools import build_template
+from ecs_composex.vpc.vpc_params import (
+    APP_SUBNETS,
+    PUBLIC_SUBNETS,
+    STORAGE_SUBNETS,
+    VPC_ID,
 )
+from troposphere.msk import Cluster as CfnMskCluster
 
-from .msk_cluster import MskCluster
+from .msk_cluster_params import MSK_CLUSTER_ADDRESSING_TYPE
 from .msk_cluster_template import build_msk_clusters
+
+
+def describe_kafka_cluster(
+    cluster: MskCluster, account_id: str, resource_id: str
+) -> dict:
+    client = cluster.lookup_session.client("kafka")
+    cluster_r = client.describe_cluster_v2(ClusterArn=resource_id)
+    return cluster_r["ClusterInfo"]
+
+
+def define_msk_clusters_mappings(module: XResourceModule, settings: ComposeXSettings):
+    for cluster in module.lookup_resources:
+        cluster.init_outputs()
+        cluster.lookup_resource(
+            MSK_CLUSTER_ARN_RE,
+            describe_kafka_cluster,
+            CfnMskCluster.resource_type,
+            "kafka:cluster",
+            use_arn_for_cloud_id=True,
+        )
+        settings.mappings[module.mapping_key].update(
+            {cluster.logical_name: cluster.mappings}
+        )
 
 
 class XStack(ComposeXStack):
@@ -28,18 +56,27 @@ class XStack(ComposeXStack):
     Class to handle MSK resources
     """
 
-    def __init__(self, title: str, settings, module: XResourceModule, **kwargs):
-        set_resources(settings, MskCluster, module)
-        x_resources = settings.compose_content[module.res_key].values()
-        lookup_resources = set_lookup_resources(x_resources)
-        if lookup_resources:
-            pass
-        new_resources = set_new_resources(x_resources, True)
-        if new_resources:
-            stack_template = build_template(f"{module.res_key} - Root Stack")
+    def __init__(
+        self, title: str, settings: ComposeXSettings, module: XResourceModule, **kwargs
+    ):
+        if module.lookup_resources:
+            if not keyisset(module.mapping_key, settings.mappings):
+                settings.mappings[module.mapping_key] = {}
+            define_msk_clusters_mappings(module, settings)
+        if module.new_resources:
+            stack_template = build_template(
+                f"{module.res_key} - Root Stack",
+                [
+                    VPC_ID,
+                    STORAGE_SUBNETS,
+                    APP_SUBNETS,
+                    PUBLIC_SUBNETS,
+                ],
+            )
             super().__init__(title, stack_template, **kwargs)
-            build_msk_clusters(new_resources, stack_template)
+            build_msk_clusters(module, self)
+            self.parent_stack = settings.root_stack
         else:
             self.is_void = True
-        for resource in x_resources:
+        for resource in module.lookup_resources:
             resource.stack = self
